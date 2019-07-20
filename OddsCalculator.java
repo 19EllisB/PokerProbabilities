fef51c0db4e1c5bd21e41696723c0cd77ed368a5
brent <19ellisb@flintridgeprep.org>
2019-07-19 21:16:02 -0700

    big nessts

--- OddsCalculator.java
+++ OddsCalculator.java
@@ -29,7 +29,7 @@ class OddsCalculator {
     }
     
     void appraiseHands() { //takes a 7 card hand and evaluates its value as a 5 card hand 
-        for (int i = 0; i < sevenCardHands.size(); i++) { //for each hand in 7CardHands
+        for (int i = 0; i < sevenCardHands.size(); i++) { //for each hand in 7CardHands, ie for each player
             if (sevenCardHands.get(i).inDeck.size() != 7) { //if the size of the hand != 7
                 System.out.println("I can't appraise a hand of not 7 cards :( ");
             } else { 
@@ -42,16 +42,46 @@ class OddsCalculator {
                 boolean isPair = false;
                 
                 //checks for flushes first
-                int diamonds = 0;
+                int diamonds = 0; //counts the number of cards in each suit
                 int clubs = 0;
                 int hearts = 0;
                 int spades = 0;
                 for (Card c: sevenCardHands.get(i).inDeck) {
-                    
+                    switch (c.suitValue) {
+                        case Card.DIAMONDS:
+                            diamonds++;
+                            break;
+                        case Card.CLUBS:
+                            clubs++;
+                            break;
+                        case Card.HEARTS:
+                            hearts++;
+                            break;
+                        case Card.SPADES:
+                            spades++;
+                            break;
+                        default:
+                        System.out.println("The for loop which checks for flushes in OddsCalculator broke");
+                    }
+                }
+                if (diamonds >= 5 ||
+                    clubs >= 5 ||
+                    hearts >= 5 ||
+                    spades >= 5) {
+                        isFlush = true;
                 }
                 
                 //Sorts the 7 Card hand in order of rankValue, highest to lowest [Ace = 14 not 1]
                 Collections.sort(sevenCardHands.get(i).inDeck, new SortByRank());
+                
+                //checks for straights
+                for (int j = 0; j < 3; j++) { //for each possible group of five ordered cards
+                    for (int k = 0; k < 4; k++) { //for the first 4 cards in that grouping
+                        if (sevenCardHands.get(i).inDeck.get(j + k).rankValue - sevenCardHands.get(i).inDeck.get(j + k + 1).rankValue == 1) {
+                            
+                        }
+                    }
+                }
             }
         }
     }
